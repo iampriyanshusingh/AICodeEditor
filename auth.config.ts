f269c9f92d7dc3cd8ae2b48cd5c3ca01adfe59f7
Priyanshu Singh <singhpriyanshu086@gmail.com>
2026-07-19 23:14:00 +0530

    `authentication done`

--- auth.config.ts
+++ auth.config.ts
@@ -3,15 +3,18 @@ import Google from "next-auth/providers/google";
 
 import type { NextAuthConfig } from "next-auth";
 
-export default{
-    providers:[
-        GitHub({
-            clientId: process.env.AUTH_GITHUB_ID,
-            clientSecret: process.env.AUTH_GITHUB_SECRET
-        }),
-        Google({
-            clientId: process.env.AUTH_GOOGLE_ID,
-            clientSecret: process.env.AUTH_GOOGLE_SECRET
-        })
-    ]
-} satisfies NextAuthConfig
+export default {
+  providers: [
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+    }),
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+} satisfies NextAuthConfig;
